7a787d6b80a4289ccdb5252a78f52ad0e9ec1397
nlegier <noalegier@gmail.com>
2022-02-22 10:19:12 +0100

    test 3

---
+++ app/src/main/java/com/example/projet_quiestce_legier_navarro_sahinidis/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.example.projet_quiestce_legier_navarro_sahinidis;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
