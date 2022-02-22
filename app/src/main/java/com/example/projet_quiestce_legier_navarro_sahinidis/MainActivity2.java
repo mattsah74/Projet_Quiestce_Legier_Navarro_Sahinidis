49385142d417028b49e703f9c0ac7dc769570511
Anthony <navarro.anthony020302@gmail.com>
2022-02-22 10:29:14 +0100

    anyufdy

--- app/src/main/java/com/example/projet_quiestce_legier_navarro_sahinidis/MainActivity2.java
+++ app/src/main/java/com/example/projet_quiestce_legier_navarro_sahinidis/MainActivity2.java
@@ -9,7 +9,7 @@ public class MainActivity2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(B.layout.activity_main2);
     }
 }
 
