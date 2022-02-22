7459898a2f1b7fe1844736057c523d7c5dbcb9eb
matth <matthieu.sahinidis@epfedu.fr>
2022-02-22 10:48:50 +0100

    rajout de la classe image

---
+++ app/src/main/java/com/example/projet_quiestce_legier_navarro_sahinidis/Image.java
@@ -0,0 +1,7 @@
+package com.example.projet_quiestce_legier_navarro_sahinidis;
+
+public class image {
+    String categorie[] = {"Acteur", "Acteur X", "Peufien", "Youtubers","TDB", "Sportif","Chanteur", "Perso de film"}
+
+            p
+    }
